0e05c26a86c18fcba1b2b37c84ae3eb6b933ee69
Shiqi Liu <liushiqi17@mails.ucas.ac.cn>
2019-09-07 21:27:45 +0800

    add support for simulation, bitstream, board.

--- prj01-beginning/run.sh
+++ prj01-beginning/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-PARAM=$1
-
-if [ "$PARAM" == "" ]; then
+if [ "$#" -eq 0 ]; then
     PARAM="open"
+fi
+
+if [ "$#" -ge 0 ]; then
+    for vars in $@; do
+        PARAM="$PARAM $vars"
+    done
 fi
 
 vivado -nojournal -nolog -mode batch -source "$(pwd)/scripts/run.tcl" -notrace -tclarg $PARAM
