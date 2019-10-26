da91231da4e95cb2aa204dd777f75c320cab31a3
陆凌天 <1037192439@qq.com>
2019-10-26 22:33:43 +0800

    debug

--- prj03-mips_cpu/src/design/cpu_core/coprocessor_params.svh
+++ prj03-mips_cpu/src/design/cpu_core/coprocessor_params.svh
@@ -12,7 +12,7 @@ package coprocessor0_params;
   } WBToCP0Data;
 
   typedef struct packed {
-    logic [7:0] zero1;
+    logic [8:0] zero1;
     logic bev;
     logic [5:0] zero2;
     logic [7:0] mask;
